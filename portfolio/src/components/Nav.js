cde165fe7cc58bb4d233d817ea4d6fe2287adb8a
kmcdonald <kmcdusa@gmail.com>
2024-03-28 00:15:01 -0400

    Good for now

--- portfolio/src/components/Nav.js
+++ portfolio/src/components/Nav.js
@@ -35,8 +35,8 @@ function Nav() {
             </nav>
             <div className="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
             <div className="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                <h5 className="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div className="offcanvas-body">
                 <div>
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -3,6 +3,10 @@ import "../component-styles/Projects.css"
 import AOS from 'aos';
 import "aos/dist/aos.css"
 import { TypeAnimation } from 'react-type-animation';
+import ReactPlayer from 'react-player';
+import FPLDashDemo from "../static/FPL-Dash-Demo.mp4";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Projects() {
     useEffect(() => {
@@ -23,30 +27,41 @@ function Projects() {
                         deletionSpeed={100000000000}
                     />
                 </h4>
-                <p className="text-center mt-3 mb-5 project-description" data-aos="fade-up" data-aos-duration="2000">Although I don't have many yet, below are the personal projects I have developed! My goal is to try out something new with each project I work on whether that be a new language, framework, or some other tech. If you want to see what I have worked on (even if it's not finished), check out my Github</p>
+                <p className="text-center mt-3 mb-5 project-description" data-aos="fade-up" data-aos-duration="2000">Although I don't have many yet, below are the personal projects I have developed! My goal is to try out something new with each project I work on whether that be a new language, 
+                framework, or some other tech. If you want to see what I have worked on (even if it's not finished), check out my <a href="https://github.com/kevkevmcd">Github <FontAwesomeIcon icon={faGithub}/></a></p>
                 <div className="row pt-5">
                     <div className="col-12 col-md-6 col-lg-6 aboutTextPart text-white" data-aos = "fade-up-right">
                         <div>
                             <h2>FPL-Dashboard</h2>
                             <hr />
-                            <p>
+                            <p className="project-description-indented ">
                                 A Fantasy English Premier League dashboard created using Python with the Flask framework. My friends and I have had a fantasy league going for a few years now, 
                                 so I decided to build out a dashboard website to better display the league data!
                                 <br />
-                                I chose to use Python to really take full advantage of the data analytical libraries
-                                such as Pandas. When it came time to deploy/host the website, I also decided to use Docker and Render.com.
+                                I chose to use Python to really take full advantage of libraries
+                                such as Pandas. When it came time to deploy/host the website, I also decided to use Docker to run it in a container.
                             </p>
                             <p style={{ color: "#6e6e6e"}}>League Code: 507</p>
-                            <a href="https://fpl-dashboard-zyxb.onrender.com/">https://fpl-dashboard-zyxb.onrender.com/</a>
+                            <a href="https://fpl-dashboard-zyxb.onrender.com/" className="icon-link">
+                                https://fpl-dashboard-zyxb.onrender.com/
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
+                                </svg>
+                            </a>
                         </div>
                     </div>
                     <div className="col-12 col-md-6 col-lg-6 mb-4 center-text" data-aos = "fade-up-left">
-                        <div className="aboutImagePart">
-                            <p>
-                                There will be a video here
-                            </p>
+                        <div className="container mt-2">
+                            <ReactPlayer url={FPLDashDemo} height='100%' width='100%' controls={true}/>
                         </div>
                         <br />                          
+                    </div>
+                </div>
+                <div className="row pt-5 text-center text-white mt-3" data-aos = "fade-up">
+                    <div className="card text-white more-projects mt-2 pt-4 pb-4">
+                        <h4>
+                            More projects to come...
+                        </h4>
                     </div>
                 </div>            
             </div>
